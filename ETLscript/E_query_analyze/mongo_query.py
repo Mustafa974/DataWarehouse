a8b182a9953ad0c0f0eaf796cf26bedeea9d2020
Mustafa <920998500@qq.com>
2018-12-30 01:19:52 +0800

    增加一些mongo查询

--- ETLscript/E_query_analyze/mongo_query.py
+++ ETLscript/E_query_analyze/mongo_query.py
@@ -1,5 +1,23 @@
 from D_data_transfer.MongoManager import *
+from bson.son import SON
 import datetime
+import pprint
+
+def movie1(col):
+    """
+    电影热度随评分的变化关系
+    :param col:
+    :return:
+    """
+    # select star, avg(review) from movie group by star order by star desc;
+    pipeline = [
+        {"$unwind": "$star"},
+        {"$group": {"_id": "$star", "avg_review": {"$avg": "$Comments"}}},
+        {"$sort": SON([("_id", -1), ("avg_review", -1)])}
+    ]
+    result = col.aggregate(pipeline)
+    # pprint.pprint(list(result))
+
 
 def time1(col):
     """
@@ -7,43 +25,56 @@ def time1(col):
     :param col:
     :return:
     """
-    dayOfWeek = ['1', '2', '3', '4', '5', '6', '7']
-    counts = [0, 0, 0, 0, 0, 0, 0]
-    for data in col.find({'releaseTime.day_of_week': {"$in": ('1', '2', '3', '4', '5', '6', '7')}}):
-        # print(data)
-        for j in range(0, 7):
-            if data['releaseTime']['day_of_week'] == dayOfWeek[j]:
-                counts[j] += 1
-                break
-    # print(counts)
+    #select day_of_week, count(1) c from time group by day_of_week order by c desc
+    pipeline = [
+        {"$unwind": "$releaseTime.day_of_week"},
+        {"$group": {"_id": "$releaseTime.day_of_week", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", -1)])}
+    ]
+    result = col.aggregate(pipeline)
+    # pprint.pprint(list(result))
 
 
 def time2(col):
     """
-    哪年电影数量最多
+    1999年有哪些电影
     :param col:
     :return:
     """
-    years = {}
-    for data in col.find({"releaseTime": {'$exists': True}}):
-        if data['releaseTime'] == '':
-            continue
-        # print(data)
-        year = data['releaseTime']
-        year = year['year']
-        # print(data['mysqlID'], year)
-        years.setdefault(year, 0)
-        years[year] += 1
-    # print(years)
+    # select year, movie_name from time where year=1999;
+    results = col.find({"releaseTime.year": 1999})
+    for result in results:
+        # print(result)
+        pass
 
 
-# def movie1(col):
+def director1(col):
     """
-    版本数最多的电影+评论数>100
+    根据名字查询导过的电影
     :param col:
     :return:
     """
+    # select name, movie_name from director where name='Jim Edward';
+    results = col.find({'directors': 'Jim Edward'})
+    for result in results:
+        # print(result)
+        pass
 
+
+# def director2(col):
+#     """
+#     找出导演电影最多的（前100）
+#     :param col:
+#     :return:
+#     """
+#     # select name, count(1) c from director group by name order by c desc limit 100;
+#     pipeline = [
+#         {"$unwind": "$directors"},
+#         {"$group": {"_id": "directors", "count": {"$sum": 1}}},
+#         {"$sort": SON([("count", -1)])}
+#     ]
+#     result = col.aggregate(pipeline)
+#     pprint.pprint(list(result))
 
 
 def main():
@@ -51,8 +82,8 @@ def main():
     col = mongo.db[config.COL_MovieWithAttr_str]
     start = datetime.datetime.now()
 
-    for i in range(10):
-        movie1(col)
+    for i in range(1):
+        director2(col)
 
     end = datetime.datetime.now()
     print(end - start)
